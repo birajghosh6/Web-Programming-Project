31f41655cdd0881a5f01f1ee14620d2d16a543a6
Biraj Ghosh <birajghosh6@gmail.com>
2020-10-05 13:41:37 +0530

    Minor changes

--- frontend/src/components/RatedMovies.js
+++ frontend/src/components/RatedMovies.js
@@ -15,28 +15,51 @@ export default class RatedMovies extends Component {
         notAuthorized()
         :
         (
-            <div
-                style = {{
+            this.props.ratings.length===0?
+            (
+                <div style = {{
                     backgroundColor: '#FFFF99',
-                    //position: 'fixed',
+                    position: 'fixed',
                     height: '100%',
-                    width: '100%'
-                }}
-            >
-                <div style={{
+                    width: '100%',
+                    fontSize: '30px',
+                    paddingLeft: 'auto',
+                    paddingRight: 'auto',
+                    paddingTop: 'auto',
+                    paddingBottom: 'auto'
+                }}>
+                   {/* <center>Loading.....</center> */}
+                    <br/>
+                    <center>
+                        You have not rated any movies {" "}
+                        <span style={{color: 'blue', fontWeight: 'bold'}}>{this.props.userId}</span>
+                    </center>
+                </div>
+            )
+            :
+            (
+                <div style = {{
                         backgroundColor: '#FFFF99',
                         //position: 'fixed',
-                        width: '100%',
                         height: '100%',
-                        marginTop: '0px'
-                        }} 
-                >
-                    {this.props.ratedMovies.map(
-                    movieItem => (
-                        <RatingItem key={movieItem.movieId} movieItem= {movieItem} ratings={this.props.ratings} />
-                    ))}
+                        width: '100%'
+                    }}>
+
+                    <div style={{
+                            backgroundColor: '#FFFF99',
+                            //position: 'fixed',
+                            width: '100%',
+                            height: '100%',
+                            marginTop: '0px'
+                            }}>
+                        {this.props.ratedMovies.map(
+                        movieItem => (
+                            <RatingItem key={movieItem.movieId} movieItem= {movieItem} ratings={this.props.ratings} />
+                        ))}
+                    </div>
+                    
                 </div>
-            </div>
+            )
         );
     }
 }
